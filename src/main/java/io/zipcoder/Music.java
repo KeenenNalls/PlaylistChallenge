cd25bd5f541140ee6fabd051068abb6ee673e15c
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-07-17 09:31:44 -0400

    finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -2,22 +2,44 @@ package io.zipcoder;
 
 public class Music {
 
+
+
     private String[] playList;
 
     public Music(String[] playList) {
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection) {
-        String track = "cecelia";
-        if (startIndex >= 2 && startIndex < playList.length) {
-            return startIndex;
-        } else {
-            return +1;
+        int counterUp = 0;
+        int counterDown = 0;
+        int i = startIndex;
+        int j = startIndex;
+
+        while (!playList[i].equals(selection)) {
+            if (i == playList.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+            counterUp++;
         }
+
+        while (!playList[j].equals(selection)) {
+            if (j == 0) {
+                j = playList.length - 1;
+            } else {
+                j--;
+            }
+            counterDown++;
+        }
+
+        if (counterDown < counterUp) {
+            return counterDown;
+        }
+        return counterUp;
     }
 }
-
-
 
 
