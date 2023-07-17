f73fe11c9a0a9e489884059571ad19747b7ef8e8
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-07-16 22:05:53 -0400

    first commit

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,20 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        String track = "cecelia";
+        if (startIndex >= 2 && startIndex < playList.length) {
+            return startIndex;
+        } else {
+            return +1;
+        }
     }
 }
+
+
+
+
